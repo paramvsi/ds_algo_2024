421deb04c0034bcfa2dc71a09e14b9de191f717a
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-06-11 19:37:25 +0530

    String isomorphic code fixed

--- src/sheet/strings/S29CheckIfTwoStringAreIsomorphic.java
+++ src/sheet/strings/S29CheckIfTwoStringAreIsomorphic.java
@@ -1,50 +1,35 @@
 package sheet.strings;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class S29CheckIfTwoStringAreIsomorphic {
 
-    public static boolean areIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        Map<Character, Character> sToTMapping = new HashMap<>();
-        Map<Character, Character> tToSMapping = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-
-            if (sToTMapping.containsKey(sChar)) {
-                if (sToTMapping.get(sChar) != tChar) {
-                    return false; // Conflicting mapping
-                }
-            } else {
-                sToTMapping.put(sChar, tChar);
-            }
-
-            if (tToSMapping.containsKey(tChar)) {
-                if (tToSMapping.get(tChar) != sChar) {
-                    return false; // Conflicting reverse mapping
-                }
-            } else {
-                tToSMapping.put(tChar, sChar);
+    public static boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()) return false;
+       
+        HashMap<Character,Character> map = new HashMap<>();
+        char sChar, tChar;
+        for(int i=0; i<s.length(); i++){
+            sChar = s.charAt(i);
+            tChar = t.charAt(i);
+            if(map.containsKey(sChar)){
+                if(map.get(sChar) != tChar) return false;
+            }else{
+                if(map.containsValue(tChar)) return false;
+                map.put(sChar, tChar);
             }
         }
-
         return true;
     }
 
     public static void main(String[] args) {
         String s = "egg";
         String t = "add";
-        System.out.println("Are strings \"" + s + "\" and \"" + t + "\" isomorphic? " + areIsomorphic(s, t));
+        System.out.println("Are strings \"" + s + "\" and \"" + t + "\" isomorphic? " + isIsomorphic(s, t));
 
         String s2 = "foo";
         String t2 = "bar";
-        System.out.println("Are strings \"" + s2 + "\" and \"" + t2 + "\" isomorphic? " + areIsomorphic(s2, t2));
+        System.out.println("Are strings \"" + s2 + "\" and \"" + t2 + "\" isomorphic? " + isIsomorphic(s2, t2));
     }
 }
 
